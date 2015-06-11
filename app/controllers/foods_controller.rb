03269509d092c1081ea5dd19e8283f95fd259468
Brian Newton <notthemessiah63@gmail.com>
2015-06-11 16:37:52 +0100

    Added new food method and some more CSS

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -11,6 +11,16 @@ class FoodsController < ApplicationController
     @food = Food.new
   end
 
+  def create
+    food = Food.create params[:food].permit(:name, :style_note)
+    if food.save
+      redirect_to foods_path
+    else
+      render 'new'
+    end
+  end
+
+
   private
   def food_params
     params.require(:food).permit(:name, :style_note)
